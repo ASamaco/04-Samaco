ba892236e207af941055a2c27077c1100205b690
ASamaco <97992558+ASamaco@users.noreply.github.com>
2022-02-14 19:08:52 -0700

    Initial commit

---
+++ plot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from math import pi
+
+import matplotlib
+matplotlib.use('agg')
+
+from matplotlib import pyplot as plt
+
+ns = []
+vs = []
+
+for n in [100, 200, 400, 800, 1600, 3200, 6400]:
+    with open(f'pi-{n}.txt') as f:
+        v = [float(l) for l in f]
+    ns.append(n)
+    vs.append(sum(v) / len(v))
+
+es = [abs(v - pi) for v in vs]
+
+plt.loglog(ns, es)
+plt.loglog(ns, [0.1 * n**-0.5 for n in ns])
+plt.savefig('pi.png', dpi=144)
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for n in 100 200 400 800 1600 3200 6400; do
+    echo $n
+    for j in {1..256}; do
+	./pi.py $n
+    done > pi-${n}.txt
+done
+
+./plot.py
